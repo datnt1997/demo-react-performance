f031013cc2900be772961906125164e02cf2e130
Dat Nguyen <datnt1997@gmail.com>
2023-10-22 19:45:58 +0700

    init project

--- src/AddButton.js
+++ src/AddButton.js
@@ -1,27 +1,28 @@
 import React from "react";
 
-export class AddButton extends React.Component {
-  render() {
-    const { onClick } = this.props;
+function AddButtonComponent(props) {
+  const { onClick } = props;
 
-    return (
-      <button
-        onClick={onClick}
-        style={{
-          height: 100,
-          width: 100,
-          borderRadius: 50,
-          backgroundColor: "#333",
-          position: "absolute",
-          bottom: 100,
-          right: 100,
-          fontSize: 20,
-          color: "#fff",
-          cursor: "pointer"
-        }}
-      >
-        Add
-      </button>
-    );
-  }
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+        backgroundColor: "#333",
+        position: "absolute",
+        bottom: 100,
+        right: 100,
+        fontSize: 20,
+        color: "#fff",
+        cursor: "pointer",
+      }}
+    >
+      Add
+    </button>
+  );
 }
+
+export const AddButton = React.memo(AddButtonComponent);
+
